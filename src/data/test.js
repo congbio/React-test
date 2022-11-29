8701a32c4c356c64dcd0fd170be2e5fae30ed23b
acongkplpe@gmail.com <acongkplpe@gmail.com>
2022-11-29 17:58:19 +0700

    file test api

---
+++ src/data/test.js
@@ -0,0 +1,52 @@
+[
+    {
+     "id": "1",
+     "title": "SHonda SH 2022",
+     "desc": "Cách trung tâm 30 km",
+     "price": "VND 180.000/ngày",
+     "latitude": 16.06500833738526,
+     "longitude": 108.24185635401889,
+     "owner": "Chùm cho thuê xe máy",
+     "img": "https://cdn.honda.com.vn/motorbikes/August2021/8biyVnv0M5aF1hisXpcU.jpg"
+    },
+    {
+     "id": "2",
+     "latitude": 16.06245812554927,
+     "longitude": 108.23813078200573,
+     "owner": "Thuê xe máy Đà Nẵng giao tận nơi",
+     "title": "Honda SH 2022",
+     "desc": "Cách trung tâm 30 km",
+     "price": "VND 50.000/ngày",
+     "img": "https://image.bnews.vn/MediaUpload/Org/2020/11/27/den-800x500px.jpg"
+    },
+    {
+     "id": "3",
+     "title": "Honda SH 2022",
+     "latitude": 16.05730855972186,
+     "longitude": 108.24405086904034,
+     "owner": "Cho thuê xe máy Đà Nẵng - Long Anh",
+     "desc": "Cách trung tâm 30 km",
+     "price": "VND 120.000/ngày",
+     "img": "https://linkhay.mediacdn.vn/storage/users/kuongngann/original/2021/06/28/238525.7FN9H60d9390840382.jpeg"
+    },
+    {
+     "id": "4",
+     "latitude": 16.06487124419325,
+     "longitude": 108.23741470492627,
+     "owner": "cho thuê xe máy tại đà nẵng",
+     "title": "SHonda SH 2022",
+     "desc": "Cách trung tâm 30 km",
+     "price": "VND 1003.000/ngày",
+     "img": "https://autopress.vn/uploads/userfiles/images/news_1555285792/honda-sh300i-2019.jpg"
+    },
+    {
+     "id": "5",
+     "latitude": 16.06487124419325,
+     "longitude": 90.23741470492627,
+     "title": "Honda SH 2022",
+     "desc": "Cách trung tâm 30 km",
+     "owner": "cho thuê xe máy tại nha",
+     "price": "VND 120.000/ngày",
+     "img": "https://cdnimg.vietnamplus.vn/uploaded/qrndqxjwp/2022_06_10/hondash20214_1.jpeg"
+    }
+   ]
